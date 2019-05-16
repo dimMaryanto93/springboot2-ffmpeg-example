008b35ffbd4550c34fa1f9a6120cde89fc04e109
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-05-16 14:27:44 +0700

    update configuration

--- src/main/java/com/maryanto/dimas/example/SpringFFmpegApplication.java
+++ src/main/java/com/maryanto/dimas/example/SpringFFmpegApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FFMPEGApplication {
+public class SpringFFmpegApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FFMPEGApplication.class, args);
+		SpringApplication.run(SpringFFmpegApplication.class, args);
 	}
 
 }
