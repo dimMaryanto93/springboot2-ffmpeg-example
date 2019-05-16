9a8e797fa7f4b3823e579ace77f8c22a17596854
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-05-16 09:46:03 +0700

    update upload video

---
+++ src/main/java/com/maryanto/dimas/example/Springboot2FfmpegCompressionApplication.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2FfmpegCompressionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2FfmpegCompressionApplication.class, args);
+	}
+
+}
